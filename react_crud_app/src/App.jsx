e812bd86e73f1a6af703b141980e01bee96fe7eb
dito-sarker <diptosarker802@gmail.com>
2022-05-01 11:50:23 +0600

    Final Commit

--- react_crud_app/src/App.jsx
+++ react_crud_app/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { Routes,Route,Link, BrowserRouter} from 'react-router-dom';
 import './App.css'
+import Create from './Components/Create/Create';
 import Delete from './Components/Delete/Delete';
 import Home from './Components/Home/Home';
+import Edit from './Components/Edit/Edit';
 import Read from './Components/Read/Read';
 import { UserProvider } from './Components/UserContext/UserContext';
 
@@ -14,6 +16,10 @@ function App() {
      <UserProvider>
       <BrowserRouter>
        <Routes>
+
+        <Route path="/edit/:id" element={<Edit/>}/>
+
+        <Route path="/create/" element={<Create/>}/>
 
         <Route path="/read/:id" element={<Read/>}/>
 
